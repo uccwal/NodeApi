ea2d8b85a5e788a3f9bb5210370bfea16ba4e36c
uccwall <uccwall@naver.com>
2023-10-24 12:46:32 +0900

    cors error npm install cors
    app.js
    
    var cors = require('cors');
    app.use(cors());

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 
 var indexRouter = require('./routes/index');
@@ -27,6 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/sampleGet', sampleGet);
